ec0ae7d097574bf462132e59fcd8c660157377aa
ZuperZee <ole.kristian.kaaseth@gmail.com>
2022-10-09 22:09:56 +0200

    ui: add button styling

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,10 @@
 import { JSX } from "solid-js";
 
 export function Button(props: JSX.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <button {...props} />;
+  return (
+    <button
+      class="border-neutrals-medium-75 mt-2 flex w-full items-center rounded border-2 bg-white p-2 outline-none focus-within:border-black"
+      {...props}
+    />
+  );
 }
