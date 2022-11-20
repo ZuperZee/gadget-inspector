bc3aa0fa179ac9dc04ec21801fc34657181c5c99
ZuperZee <ole.kristian.kaaseth@gmail.com>
2022-11-20 21:42:21 +0100

    ui: update switch

--- src/App.tsx
+++ src/App.tsx
@@ -119,10 +119,8 @@ const App: Component = () => {
             class="peer sr-only"
             onChange={(e) => setIsByteSwap(e.currentTarget.checked)}
           />
-          <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800" />
-          <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-            Byte swap
-          </span>
+          <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300" />
+          <span class="ml-3 text-sm font-medium text-gray-900">Byte swap</span>
         </label>
         <label class="relative mt-2 flex cursor-pointer  items-center">
           <input
@@ -131,10 +129,8 @@ const App: Component = () => {
             class="peer sr-only"
             onChange={(e) => setIsWordSwap(e.currentTarget.checked)}
           />
-          <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800" />
-          <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-            Word swap
-          </span>
+          <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300" />
+          <span class="ml-3 text-sm font-medium text-gray-900">Word swap</span>
         </label>
         <Button
           onClick={() => {
