5ff3a1c805ca5596c2d028ade533c48d3d4bc6c6
ZuperZee <ole.kristian.kaaseth@gmail.com>
2022-11-26 20:40:41 +0100

    chore: simplify prettier config Don't format tailwindcss or organize imports

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,25 +1,4 @@
-const tailwind = require("prettier-plugin-tailwindcss");
-const organizeImports = require("prettier-plugin-organize-imports");
-
-const combinedFormatter = {
-  ...tailwind,
-  parsers: {
-    ...tailwind.parsers,
-    ...Object.keys(organizeImports.parsers).reduce((acc, key) => {
-      acc[key] = {
-        ...tailwind.parsers[key],
-        preprocess(code, options) {
-          return organizeImports.parsers[key].preprocess(code, options);
-        },
-      };
-      return acc;
-    }, {}),
-  },
-};
-
 /**
  * @type {import('prettier').Config}
  */
-module.exports = {
-  plugins: [combinedFormatter],
-};
+module.exports = {};
